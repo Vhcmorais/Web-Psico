729221fd06acaf542f31ce97b97ab24efeecfc7b
Vhcmorais <vhcmoraiss@yahoo.com>
2026-04-02 23:14:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+/* ================= JS (script.js) ================= */
+
+// menu mobile
+
+document.getElementById("menuToggle").addEventListener("click", function(){
+  document.querySelector("nav ul").classList.toggle("active");
+});
+
+// animação scroll
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", reveal);
+
+const track = document.getElementById("carouselTrack");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let index = 0;
+
+function updateCarousel() {
+  if (!track) return;
+  const width = track.clientWidth;
+  track.style.transform = `translateX(-${index * width}px)`;
+}
+
+nextBtn?.addEventListener("click", () => {
+  const total = track.children.length;
+  index = (index + 1) % total;
+  updateCarousel();
+});
+
+prevBtn?.addEventListener("click", () => {
+  const total = track.children.length;
+  index = (index - 1 + total) % total;
+  updateCarousel();
+});
+
+window.addEventListener("resize", updateCarousel);
+
+// animação inicial
+reveal();
